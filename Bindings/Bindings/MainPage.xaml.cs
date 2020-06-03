19bfc055ff6598b8c5bcfbbf0fa437d26e3ef2d1
diego12 <diego.anchillo@tecsup.edu.pe>
2020-06-02 21:52:18 -0500

    one commit

---
+++ Bindings/Bindings/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bindings
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ViewToViewDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ViewToViewDemo());
+            };
+            BindingModeDemo.Clicked += async (sender, e) => {
+                await Navigation.PushAsync(new BindingModeDemo());
+            };
+            ListViewDemo.Clicked += async (sender, e) => {
+                await Navigation.PushAsync(new ListViewDemo());
+            };
+            PickerDemo.Clicked += async (sender, e) => {
+                await Navigation.PushAsync(new PickerDemo());
+            };
+        }
+    }
+}
